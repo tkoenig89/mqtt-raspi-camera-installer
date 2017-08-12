079a3652ba9ee78465aaa96be97a6c87bb341a7e
Tobias König <tkoenig89@users.noreply.github.com>
2017-08-12 12:24:54 +0200

    installing git added

--- prerequisits.sh
+++ prerequisits.sh
@@ -1,8 +1,5 @@
 #add node repository to apt-get
 curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
 
-#install node
-sudo apt install -y nodejs
-
-#install graphicsmagick
-sudo apt-get install graphicsmagick
+#install git, node, graphicsmagick
+sudo apt install -y git nodejs graphicsmagick
